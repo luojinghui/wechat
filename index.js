c0f3a633d5b8a7ac7ae7f0dc88b1f7b21732e5c6
luojinghui <luojinghui@xylink.com>
2018-05-18 11:33:52 +0800

    modify

--- index.js
+++ index.js
@@ -3,30 +3,63 @@
  * @date 2018/5/16
  * @Description:
  */
-
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var fs = require('fs');
+var path = './history.json';
+var count = [];
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
+	res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
+	console.log('A user connected');
+	count.push(socket.id);
+	io.emit('count', count.length);
 
-  socket.on('chat message', function(msg){
+	socket.on('chat message', function(msg) {
+		console.log('message: ' + JSON.stringify(msg));
 
-    console.log('message: ' + msg);
+		let result = JSON.parse(fs.readFileSync(path)) || [];
+		if (result.length > 50) {
+			result.length = 50;
+		}
+		result.push(msg);
 
-    io.emit('chat message', msg);
-  });
+		fs.writeFile(path, JSON.stringify(result), function(err) {
+			if (err) console.log('write fail, please check it!');
+		});
 
-  socket.on('disconnect', () => {
-    console.log('user disconnected!');
-  })
+		io.emit('chat message', msg);
+	});
+
+	socket.on('get data', function(data) {
+		console.log('A user coming: ', JSON.stringify(data));
+
+		let result = JSON.parse(fs.readFileSync(path));
+
+		io.to(socket.id).emit('get data', result);
+	});
+
+	socket.on('disconnect', () => {
+		console.log('user disconnected!');
+
+		io.emit('count', count.length);
+		count.splice(del(count, socket.id), 1);
+	});
 });
 
+function del(count, id) {
+	let len = count.length;
+	for (let i = 0; i < len; i++) {
+		if (count[i] === id) return i;
+	}
+
+	return -1;
+}
+
 http.listen(9999, () => {
-  console.log('listening on *:9999');
+	console.log('listening on *:9999');
 });
